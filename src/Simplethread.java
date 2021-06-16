ed85fd6cb7e69228f0e0b7132a311c05a813a358
MohamedAbusharkh <abusham1@ATC-177-05.ferris.local>
2021-06-16 10:52:34 -0400

    explained what a the file does

--- src/Simplethread.java
+++ src/Simplethread.java
@@ -1,6 +1,6 @@
 
 public class Simplethread extends Thread {
-	 public void run(){
+	 public void run(){ // this handles a simple thread
 	       System.out.println("One more thread running   "+System.currentTimeMillis());
 	    }
 	  
